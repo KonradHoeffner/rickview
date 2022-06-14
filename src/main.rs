ba30e01f1e8fbd0679e2e15814c2862836ce6dfb
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-06-14 15:36:27 +0200

    Add actix-web.

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,19 @@
-use sophia::graph::{inmem::FastGraph, *};
-use sophia::ns::Namespace;
-use sophia::parser::turtle;
-use sophia::triple::stream::TripleSource;
-use sophia::triple::Triple;
-use std::fs::File;
-use std::io::BufReader;
-use sophia::prefix::{PrefixMap,PrefixBox,Prefix};
-use sophia::iri::{Iri,IriBox};
-use sophia::term::Term;
-use std::rc::Rc;
+mod page;
 
-fn prefix_term(prefixes: &Vec<(PrefixBox,IriBox)>, term: &Term<Rc<str>>) -> String
-{
-    let suffix = prefixes.get_prefixed_pair(term);
-        let s = match suffix
-        {
-            Some(x) => x.0.to_string() +":"+ &x.1.to_string(),
-            None => term.to_string().replace(['<','>'],""),
-        };
-        return s;
+use actix_web::{get, web, App, HttpServer, Responder};
+use page::page;
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    page("")
 }
 
-fn add_prefix(mut prefixes: Vec<(PrefixBox,IriBox)>, prefix : &str, iri: &str) -> Vec<(PrefixBox,IriBox)>
-{
-    let prefix_box: PrefixBox = Prefix::new(prefix).unwrap().boxed();
-    let iri_box: IriBox = Iri::new(iri).unwrap().boxed();
-    prefixes.push((prefix_box,iri_box));
-    return prefixes;
-}
-
-fn main() {
-    let file = File::open("hito.nt").expect("Unable to open file");
-    let reader = BufReader::new(file);
-    let graph: FastGraph = turtle::parse_bufread(reader).collect_triples().unwrap();
-    
-    let mut prefixes: Vec<(PrefixBox,IriBox)> = Vec::new();
-    prefixes = add_prefix(prefixes,"hito","http://hitontology.eu/ontology/");
-    prefixes = add_prefix(prefixes,"purl","http://purl.org/vocab/vann/");
-    prefixes = add_prefix(prefixes,"rdf","http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-    prefixes = add_prefix(prefixes,"rdfs","http://www.w3.org/2000/01/rdf-schema#");
-    prefixes = add_prefix(prefixes,"owl","http://www.w3.org/2002/07/owl#");
-    prefixes = add_prefix(prefixes,"skos","http://www.w3.org/2004/02/skos/core#");
-    prefixes = add_prefix(prefixes,"skos","http://www.w3.org/2004/02/skos/core#");
-    prefixes = add_prefix(prefixes,"sh","http://www.w3.org/ns/shacl#");
-
-    let hito_ns = Namespace::new("http://hitontology.eu/ontology/").unwrap();
-
-    let subject = hito_ns.get("SoftwareProduct").unwrap();
-
-    println!("<html><body>");
-    println!("<h1>{}</h1>",subject.to_string().replace(['<','>'],""));
-    let results = graph.triples_with_s(&subject);
-     print!("<ul>");
-    for res in results {
-        let t = res.unwrap();
-         println!("<li>{} {}</li>",prefix_term(&prefixes,t.p()),prefix_term(&prefixes,t.o()));
-    }
-    print!("</ul>");
-    println!("## Inverse");
-    let results = graph.triples_with_o(&subject);
-    print!("<ul>");
-    for res in results {
-        let t = res.unwrap();
-         println!("<li>is {} of {}</li>",prefix_term(&prefixes,t.p()),prefix_term(&prefixes,t.s()));
-    }
-    print!("</ul>");
-    println!("</body></html>");
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
---
+++ src/page.rs
@@ -0,0 +1,69 @@
+use sophia::graph::{inmem::FastGraph, *};
+use sophia::ns::Namespace;
+use sophia::parser::turtle;
+use sophia::triple::stream::TripleSource;
+use sophia::triple::Triple;
+use std::fs::File;
+use std::io::BufReader;
+use sophia::prefix::{PrefixMap,PrefixBox,Prefix};
+use sophia::iri::{Iri,IriBox};
+use sophia::term::Term;
+use std::rc::Rc;
+
+fn prefix_term(prefixes: &Vec<(PrefixBox,IriBox)>, term: &Term<Rc<str>>) -> String
+{
+    let suffix = prefixes.get_prefixed_pair(term);
+        let s = match suffix
+        {
+            Some(x) => x.0.to_string() +":"+ &x.1.to_string(),
+            None => term.to_string().replace(['<','>'],""),
+        };
+        return s;
+}
+
+fn add_prefix(mut prefixes: Vec<(PrefixBox,IriBox)>, prefix : &str, iri: &str) -> Vec<(PrefixBox,IriBox)>
+{
+    let prefix_box: PrefixBox = Prefix::new(prefix).unwrap().boxed();
+    let iri_box: IriBox = Iri::new(iri).unwrap().boxed();
+    prefixes.push((prefix_box,iri_box));
+    return prefixes;
+}
+
+pub fn page(uri: &str) -> String {
+    let file = File::open("hito.nt").expect("Unable to open file");
+    let reader = BufReader::new(file);
+    let graph: FastGraph = turtle::parse_bufread(reader).collect_triples().unwrap();
+    
+    let mut prefixes: Vec<(PrefixBox,IriBox)> = Vec::new();
+    prefixes = add_prefix(prefixes,"hito","http://hitontology.eu/ontology/");
+    prefixes = add_prefix(prefixes,"purl","http://purl.org/vocab/vann/");
+    prefixes = add_prefix(prefixes,"rdf","http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+    prefixes = add_prefix(prefixes,"rdfs","http://www.w3.org/2000/01/rdf-schema#");
+    prefixes = add_prefix(prefixes,"owl","http://www.w3.org/2002/07/owl#");
+    prefixes = add_prefix(prefixes,"skos","http://www.w3.org/2004/02/skos/core#");
+    prefixes = add_prefix(prefixes,"skos","http://www.w3.org/2004/02/skos/core#");
+    prefixes = add_prefix(prefixes,"sh","http://www.w3.org/ns/shacl#");
+
+    let hito_ns = Namespace::new("http://hitontology.eu/ontology/").unwrap();
+
+    let subject = hito_ns.get("SoftwareProduct").unwrap();
+    let mut s: String = "<html><body>\n".to_owned();
+    s.push_str(&format!("<h1>{}</h1>",subject.to_string().replace(['<','>'],"")));
+    let results = graph.triples_with_s(&subject);
+     s.push_str("<ul>\n");
+    for res in results {
+        let t = res.unwrap();
+         s.push_str(&format!("<li>{} {}</li>",prefix_term(&prefixes,t.p()),prefix_term(&prefixes,t.o())));
+    }
+    print!("</ul>");
+    println!("## Inverse");
+    let results = graph.triples_with_o(&subject);
+    print!("<ul>");
+    for res in results {
+        let t = res.unwrap();
+         println!("<li>is {} of {}</li>",prefix_term(&prefixes,t.p()),prefix_term(&prefixes,t.s()));
+    }
+    print!("</ul>");
+    println!("</body></html>");
+    s
+}
