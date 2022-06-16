d788be1a83489bf7a6c9f1c7670e47319141ddf9
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-06-16 11:32:40 +0200

    Reorganize data files into new 'data' folder. Use tinytemplate library.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,48 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate tinytemplate;
 
 mod page;
 
-use actix_files::NamedFile;
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{get, guard, web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
 use page::page;
+use serde::{Serialize};
+use tinytemplate::TinyTemplate;
+use std::sync::Arc;
 
-async fn favicon(_req: HttpRequest) -> Result<NamedFile> {
-    Ok(NamedFile::open("favicon.ico")?)
+static TEMPLATE: &str = std::include_str!("../data/template.html");
+static FAVICON: &[u8; 318] = std::include_bytes!("../data/favicon.ico");
+static CSS: &str = std::include_str!("../data/rickview.css");
+
+#[get("/rickview.css")]
+async fn css() -> impl Responder {
+    HttpResponse::Ok().content_type("text/css").body(CSS)
 }
 
-async fn css(_req: HttpRequest) -> Result<NamedFile> {
-    Ok(NamedFile::open("rickview.css")?)
+#[get("/favicon.ico")]
+async fn favicon() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("image/x-icon")
+        .body(FAVICON.as_ref())
+}
+
+#[derive(Serialize)]
+struct Context {
+    title: String,
 }
 
 #[get("/ontology/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(page(name.as_ref()))
+    let mut tt = TinyTemplate::new();
+    tt.add_template("template",TEMPLATE).unwrap();
+    let body = tt.render("template", &Context {title: "blubb".to_owned()}).unwrap();
+    HttpResponse::Ok().content_type("text/html").body(body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(greet)
-            .route("/rickview.css", web::get().to(css))
-            .route("/favicon.ico", web::get().to(favicon))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(css).service(favicon).service(greet))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
