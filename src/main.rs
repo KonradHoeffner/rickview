ac2d709c1b4e4b699a674d2bbaf439ddcbf8a24f
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-08-30 14:31:18 +0200

    Add internal server error cause message.

--- src/main.rs
+++ src/main.rs
@@ -66,8 +66,8 @@ async fn resource_html(request: HttpRequest, suffix: web::Path<String>) -> impl 
                                     debug!("{} serve as HTML", prefixed);
                                     HttpResponse::Ok().content_type("text/html; charset-utf-8").body(html)
                                 }
-                                Err(_) => {
-                                    let message = format!("Internal server error. Could not render resource {}.", prefixed);
+                                Err(err) => {
+                                    let message = format!("Internal server error. Could not render resource {}:\n{}.", prefixed,err);
                                     error!("{}", message);
                                     HttpResponse::InternalServerError().body(message)
                                 }
