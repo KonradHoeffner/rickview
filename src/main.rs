d20520239260fd8e9918e91043e7dc959baad5e7
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-09-03 09:37:15 +0200

    Test GitHub actions Rust cache again.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 #![feature(once_cell)]
 //! Lightweight and performant RDF browser.
 //! An RDF browser is a web application that *resolves* RDF resources: given the HTTP(s) URL identifying a resource it returns an HTML summary.
