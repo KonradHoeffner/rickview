e8778d392c8044c5e1277020b64b2237db536305
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-04-04 10:58:04 +0200

    Remove once_cell feature attribute as it has been stable since 1.70.0-nightly.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@
 #![allow(clippy::unused_async)]
 #![allow(clippy::similar_names)]
 #![deny(rust_2018_idioms)]
-#![feature(once_cell)]
 #![feature(let_chains)]
 //! Lightweight and performant RDF browser.
 //! An RDF browser is a web application that *resolves* RDF resources: given the HTTP(s) URL identifying a resource it returns an HTML summary.
