62e49b9f17259d4915a6624e490dbbb15d884a4d
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2023-04-17 10:21:15 +0200

    Deactivate unused let chain feature.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@
 #![allow(clippy::unused_async)]
 #![allow(clippy::similar_names)]
 #![deny(rust_2018_idioms)]
-#![feature(let_chains)]
+// don't use let chains until supported by rustfmt
+//#![feature(let_chains)]
 //! Lightweight and performant RDF browser.
 //! An RDF browser is a web application that *resolves* RDF resources: given the HTTP(s) URL identifying a resource it returns an HTML summary.
 //! Besides HTML, the RDF serialization formats RDF/XML, Turtle and N-Triples are also available using content negotiation.
