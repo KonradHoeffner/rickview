2cea03ff06a329bd01774897e15c2fc3d3d796e2
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2025-03-27 16:47:26 +0100

    fix: prevent missing subclass relationships in class page

--- src/classes.rs
+++ src/classes.rs
@@ -1,50 +1,55 @@
 use crate::rdf::{Piri, graph, titles};
-use sophia::api::term::SimpleTerm::{self, Iri};
+use multimap::MultiMap;
+use sophia::api::MownStr;
+use sophia::api::term::IriRef;
+use sophia::api::term::SimpleTerm::Iri;
 use sophia::api::term::matcher::Any;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
-fn node(class: &SimpleTerm<'_>, subclasses: &HashMap<SimpleTerm<'_>, SimpleTerm<'_>>) -> (String, u32) {
-    match class {
-        Iri(iri) => {
-            let piri = Piri::from(iri);
-            let title = if let Some(title) = titles().get(&piri.to_string()) { title } else { &piri.short() };
-            let mut inner = String::new();
-            let mut count = 0;
-            for (child, _) in subclasses.iter().filter(|(_, p)| *p == class) {
+type IriM<'a> = IriRef<MownStr<'a>>;
+
+fn node(class: &IriM<'_>, subclasses: &MultiMap<&IriM<'_>, &IriM<'_>>) -> (String, u32) {
+    let piri = Piri::from(class);
+    let title = if let Some(title) = titles().get(&piri.to_string()) { title } else { &piri.short() };
+    let mut inner = String::new();
+    let mut count = 0;
+    let s = match subclasses.get_vec(class) {
+        Some(children) => {
+            for child in children {
                 let (child_s, child_count) = &node(child, subclasses);
                 inner += child_s;
-                count += 1;
-                count += child_count;
+                count += child_count + 1;
             }
-            match count {
-                0 => (format!("<p style='margin: 1em;'>&bull; <a href='{}' target='_blank'>{}</p>", piri.root_relative(), title), 0),
-                _ => (
-                    format!(
-                        "<details style='margin: 1em;'><summary><a href='{}' target='_blank'>{} ({count})</summary>{inner}</details>\n",
-                        piri.root_relative(),
-                        title
-                    ),
-                    count,
-                ),
-            }
+            format!(
+                "<details style='margin: 1em;'><summary><a href='{}' target='_blank'>{} ({count})</summary>{inner}</details>\n",
+                piri.root_relative(),
+                title
+            )
         }
-        _ => {
-            //format!("<span style='color:red;'>Invalid term: {class:?} is not a class</span>")
-            (String::new(), 0)
-        }
-    }
+        None => format!("<p style='margin: 1em;'>&bull; <a href='{}' target='_blank'>{}</p>", piri.root_relative(), title),
+    };
+    (s, count)
 }
 
 pub fn class_tree() -> String {
     let g = graph();
-    let mut subclasses = HashMap::<SimpleTerm<'_>, SimpleTerm<'_>>::new();
-    for tt in g.triples_matching(Any, Some(sophia::api::ns::rdfs::subClassOf), Any) {
-        let t: [SimpleTerm<'_>; 3] = tt.expect("error fetching class triple");
-        let [child, _, parent] = t;
-        subclasses.insert(child, parent);
+    // the graphs we use should never fail
+    // rdfs:subclassOf is also used with blank nodes for owl restrictions that we ignore
+    let pairs: Vec<[IriM<'_>; 2]> = g
+        .triples_matching(Any, Some(sophia::api::ns::rdfs::subClassOf), Any)
+        .map(|t| t.expect("error fetching class triple terms"))
+        .filter_map(|t| match t {
+            [Iri(child), _, Iri(parent)] => Some([child, parent]),
+            _ => None,
+        })
+        .collect();
+    let mut subclasses = MultiMap::<&IriM<'_>, &IriM<'_>>::new();
+    let mut children = HashSet::<&IriM<'_>>::new();
+    for [child, parent] in &pairs {
+        subclasses.insert(parent, child);
+        children.insert(child);
     }
-    let parents: HashSet<_> = subclasses.values().collect();
-    let children: HashSet<_> = subclasses.keys().collect();
+    let parents: HashSet<_> = subclasses.keys().copied().collect();
     let roots = parents.difference(&children);
 
     let mut s = String::new();
